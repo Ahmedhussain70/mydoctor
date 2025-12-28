84cd93e6c597f73f924980717982c22486a70e94
Ahmed Hussain <113634512+Ahmedhussain70@users.noreply.github.com>
2025-12-28 02:01:51 +0200

    up

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\Api\AnswerController;
+use App\Http\Controllers\API\AnswerController;
 use App\Http\Controllers\API\QuestionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
